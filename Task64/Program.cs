b0cf02c3b7bb28812772b44818b2705b6fd8d53d
AleksandrBK <lebedevavlv@gmail.com>
2023-07-26 15:42:03 +0400

    Task 64

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -3,3 +3,16 @@
 // Выполнить с помощью рекурсии.
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string PrintNumbers(int start, int end) // start = 1, end = n
+{
+    // Базовый случай
+    if (start == end) return start.ToString();
+    // Рекурсивный случай - вызвать функцию внутри функции 
+    return start + ", " + PrintNumbers(start - 1, end);
+}
+
+Console.WriteLine($"Числа от {n} до 1: {PrintNumbers(n, 1)}");
